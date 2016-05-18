17471d8992d62871fcfc5daf5d0931ed0d6e9537
Vicdor <vicdor@vip.qq.com>
2016-05-19 00:20:41 +0800

    测试使用EnumUtils

--- itao-common/src/main/java/com/itao/util/EnumUtils.java
+++ itao-common/src/main/java/com/itao/util/EnumUtils.java
@@ -68,4 +68,73 @@ public class EnumUtils {
         }
         return null;
     }
+
+    enum TestEnum{
+        TEST1(1,"hi"),
+        TEST2(2,"ho");
+
+        private int key;
+        private String value;
+
+        TestEnum(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+        public void setKey(int key) {
+            this.key = key;
+        }
+        public String getValue() {
+            return value;
+        }
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+
+    enum TestEnum2{
+        TEST1(1,"hi"),
+        TEST2(2,"ho");
+
+        private int key;
+        private String content;
+
+        TestEnum2(int key, String content) {
+            this.key = key;
+            this.content = content;
+        }
+
+        public int getKey() {
+            return key;
+        }
+        public void setKey(int key) {
+            this.key = key;
+        }
+        public String getValue() {
+            return content;
+        }
+        public void setValue(String content) {
+            this.content = content;
+        }
+    }
+
+    public static void main(String[] args) {
+        /*TestEnum testEnum = valueOf(TestEnum.class,"ho");
+        if(testEnum == TestEnum.TEST2){
+            System.out.println("tttt2");
+        }*/
+
+        TestEnum2 testEnum2 = valueOf(TestEnum2.class,"content","ho");
+        if(testEnum2 == TestEnum2.TEST2){
+            System.out.println("tttt2");
+        }
+
+        TestEnum2 testEnum22 = valueOf(TestEnum2.class,"key",2);
+        if(testEnum22 == TestEnum2.TEST2){
+            System.out.println("tttt22");
+        }
+    }
 }
