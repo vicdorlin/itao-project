ae441dad26f375562ce0a46f72ea2fd7fb6bc1bb
Vicdor <vicdor@vip.qq.com>
2016-05-19 01:05:49 +0800

    EnumUtils 方法精简修正

--- itao-common/src/main/java/com/itao/util/EnumUtils.java
+++ itao-common/src/main/java/com/itao/util/EnumUtils.java
@@ -10,68 +10,37 @@ public class EnumUtils {
 
     /**
      * 枚举处理
+     *
      * @param elementType
      * @param value
      * @param <T>
      * @return
      */
-    public static <T> T valueOf(Class<T> elementType,Object value){
-        if(value==null){
-            return null;
-        }
-        if(elementType.isEnum()){
-            Object[] objs = elementType.getEnumConstants();
-            for (Object obj : objs) {
-                Method m;
+    public static <T> T valueOf(Class<T> elementType, Object value) {
+        return valueOf(elementType, "value", value);
+    }
+
+    public static <T> T valueOf(Class<T> elementType, String filedName, Object value) {
+        if(value != null && elementType.isEnum()){
+            T[] fields = elementType.getEnumConstants();
+            for (T f : fields) {
                 try {
-                    m = obj.getClass().getDeclaredMethod("values", null);
-                    Object[] result = (Object[]) m.invoke(obj, null);
-                    for(Object objOne : result){
-                        Field code = objOne.getClass().getDeclaredField("value");
-                        code.setAccessible(true);
-                        Object objVal = code.get(objOne);
-                        if(objVal.equals(value)){
-                            return (T)objOne;
-                        }
-                    }
+                    Field field = f.getClass().getDeclaredField(filedName);
+                    field.setAccessible(true);
+                    T fieldValue = (T) field.get(f);
+                    if(value.equals(fieldValue)) return f;
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+
             }
         }
         return null;
     }
 
-    public static <T> T valueOf(Class<T> elementType,String filedName,Object value){
-        if(value==null){
-            return null;
-        }
-        if(elementType.isEnum()){
-            Object[] objs = elementType.getEnumConstants();
-            for (Object obj : objs) {
-                Method m;
-                try {
-                    m = obj.getClass().getDeclaredMethod("values", null);
-                    Object[] result = (Object[]) m.invoke(obj, null);
-                    for(Object objOne : result){
-                        Field code = objOne.getClass().getDeclaredField(filedName);
-                        code.setAccessible(true);
-                        Object objVal = code.get(objOne);
-                        if(objVal.equals(value)){
-                            return (T)objOne;
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return null;
-    }
-
-    enum TestEnum{
-        TEST1(1,"hi"),
-        TEST2(2,"ho");
+    enum TestEnum {
+        TEST1(1, "hi"),
+        TEST2(2, "ho");
 
         private int key;
         private String value;
@@ -84,20 +53,23 @@ public class EnumUtils {
         public int getKey() {
             return key;
         }
+
         public void setKey(int key) {
             this.key = key;
         }
+
         public String getValue() {
             return value;
         }
+
         public void setValue(String value) {
             this.value = value;
         }
     }
 
-    enum TestEnum2{
-        TEST1(1,"hi"),
-        TEST2(2,"ho");
+    enum TestEnum2 {
+        TEST1(1, "hi"),
+        TEST2(2, "ho");
 
         private int key;
         private String content;
@@ -110,31 +82,34 @@ public class EnumUtils {
         public int getKey() {
             return key;
         }
+
         public void setKey(int key) {
             this.key = key;
         }
+
         public String getValue() {
             return content;
         }
+
         public void setValue(String content) {
             this.content = content;
         }
     }
 
     public static void main(String[] args) {
-        /*TestEnum testEnum = valueOf(TestEnum.class,"ho");
-        if(testEnum == TestEnum.TEST2){
-            System.out.println("tttt2");
-        }*/
-
-        TestEnum2 testEnum2 = valueOf(TestEnum2.class,"content","ho");
-        if(testEnum2 == TestEnum2.TEST2){
-            System.out.println("tttt2");
+        TestEnum testEnum = valueOf(TestEnum.class, "ho");
+        if (testEnum == TestEnum.TEST2) {
+            System.out.println("test for value default");
         }
 
-        TestEnum2 testEnum22 = valueOf(TestEnum2.class,"key",2);
-        if(testEnum22 == TestEnum2.TEST2){
-            System.out.println("tttt22");
+        TestEnum2 testEnum2 = valueOf(TestEnum2.class, "content", "ho");
+        if (testEnum2 == TestEnum2.TEST2) {
+            System.out.println("test for content");
+        }
+
+        TestEnum2 testEnum22 = valueOf(TestEnum2.class, "key", 2);
+        if (testEnum22 == TestEnum2.TEST2) {
+            System.out.println("test for key");
         }
     }
 }
