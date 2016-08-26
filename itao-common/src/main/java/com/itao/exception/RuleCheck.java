2e20c6eca72c33441f71d8e153e6a08c2c40cbc8
linss <linss@aixuedai.com>
2016-08-26 18:19:00 +0800

    rebuild

--- itao-common/src/main/java/com/itao/exception/RuleCheck.java
+++ itao-common/src/main/java/com/itao/exception/RuleCheck.java
@@ -1,12 +1,16 @@
 package com.itao.exception;
 
+import com.itao.util.CommonUtils;
+
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static com.itao.util.CommonUtils.isSetEmpty;
 import static com.itao.util.CommonUtils.notExist;
 
 /**
@@ -16,6 +20,7 @@ import static com.itao.util.CommonUtils.notExist;
 public final class RuleCheck {
     /**
      * 空验证
+     *
      * @param key
      * @param msg
      * @throws RuleException
@@ -28,87 +33,57 @@ public final class RuleCheck {
 
     /**
      * 简化后的空验证 1
-     * @param javaBean	类型符合JavaBean规范的java对象
-     * @param keyWords	需要进行空验证的字段名称集
-     *        (集合中元素若非javaBean中字段名称，则该元素无效，
-     *        并且会打印相关的异常堆栈信息，应尽量避免)
+     *
+     * @param javaBean 类型符合JavaBean规范的java对象
+     * @param keyWords 需要进行空验证的字段名称集
+     *                 (集合中元素若非javaBean中字段名称，则该元素无效，
+     *                 并且会打印相关的异常堆栈信息，应尽量避免)
      * @throws RuleException
      */
     public static final void checkEmpty(Object javaBean, List<String> keyWords) throws RuleException {
-        if(javaBean == null || keyWords == null || keyWords.size() == 0) return;
-        try {
-            Class clazz = javaBean.getClass();
-            for (String keyWord : keyWords) {
-                PropertyDescriptor pd = new PropertyDescriptor(keyWord, clazz);
-                Method getMethod = pd.getReadMethod();//获得keyWord在obj中对应的getter方法
-                Object o = getMethod.invoke(javaBean);
-                checkEmpty(o,keyWord);//空验证
-            }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (IntrospectionException e) {
-            e.printStackTrace();
-        }  catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
+        checkEmpty(javaBean, keyWords, null);
     }
 
     /**
      * 简化后的空验证 2
+     *
      * @param javaBean 验证所有声明的字段（包括父类中的所有字段，但不包括任何serialVersionUID)
      */
-    public static final void checkEmpty(Object javaBean) throws RuleException {
-        if(javaBean == null) return;
-        try {
-            Class clazz = javaBean.getClass();
-            Field[] fields = clazz.getDeclaredFields();
-            for (Field field : fields) {
-                String keyWord = field.getName();
-                if("serialVersionUID".equals(keyWord)) continue;
-                PropertyDescriptor pd = new PropertyDescriptor(keyWord, clazz);
-                Method getMethod = pd.getReadMethod();//获得keyWord在obj中对应的getter方法
-                Object o = getMethod.invoke(javaBean);
-                checkEmpty(o,keyWord);//空验证
-            }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (IntrospectionException e) {
-            e.printStackTrace();
-        }  catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
+    public static final <B> void checkEmpty(B javaBean) throws RuleException {
+        checkEmpty(javaBean, null, null);
     }
 
     /**
      * 简化后的空验证 3
-     * @param javaBean 类型符合JavaBean规范的java对象
+     *
+     * @param javaBean    类型符合JavaBean规范的java对象
      * @param exceptWords 不需要进行空验证的字段名称集
      * @throws RuleException
      */
-    public static final void checkEmptyWithout(Object javaBean, List<String> exceptWords) throws RuleException {
-        if(javaBean == null) return;
-        if(exceptWords == null || exceptWords.size() == 0) {
-            checkEmpty(javaBean);
-            return;
-        }
+    public static final <B> void checkEmpty(B javaBean, Set<String> exceptWords) throws RuleException {
+        checkEmpty(javaBean, null, exceptWords);
+    }
+
+    public static final <B> void checkEmpty(B javaBean, List<String> keyWords, Set<String> exceptWords) {
+        if (notExist(javaBean)) return;
+        if (isSetEmpty(keyWords)) keyWords = CommonUtils.extractFieldNames(javaBean.getClass());
+        if (isSetEmpty(keyWords)) return;
+        if (isSetEmpty(exceptWords)) exceptWords = new HashSet<>();
+        exceptWords.add("serialVersionUID");
         try {
             Class clazz = javaBean.getClass();
-            Field[] fields = clazz.getDeclaredFields();
-            for (Field field : fields) {
-                String keyWord = field.getName();
-                if("serialVersionUID".equals(keyWord)) continue;
-                if(exceptWords.indexOf(keyWord) >= 0) continue;
+            for (String keyWord : keyWords) {
+                if (exceptWords.contains(keyWord)) continue;
                 PropertyDescriptor pd = new PropertyDescriptor(keyWord, clazz);
                 Method getMethod = pd.getReadMethod();//获得keyWord在obj中对应的getter方法
                 Object o = getMethod.invoke(javaBean);
-                checkEmpty(o,keyWord);//空验证
+                checkEmpty(o, keyWord);//空验证
             }
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (IntrospectionException e) {
             e.printStackTrace();
-        }  catch (InvocationTargetException e) {
+        } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
     }
--- itao-common/src/main/java/com/itao/util/CommonUtils.java
+++ itao-common/src/main/java/com/itao/util/CommonUtils.java
@@ -3,10 +3,9 @@ package com.itao.util;
 import com.alibaba.fastjson.JSON;
 import com.itao.enums.DateFormatEnum;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Vicdor on 2016-04-30-0030.
@@ -14,18 +13,19 @@ import java.util.Map;
 public class CommonUtils {
     /**
      * 判断对象是否不为空
+     *
      * @param o
      * @return
      */
-    public static boolean exist(Object o){
-        if(o == null) return false;
-        if(o instanceof String){
+    public static boolean exist(Object o) {
+        if (o == null) return false;
+        if (o instanceof String) {
             return ((String) o).trim().length() > 0;
         }
-        if(o instanceof Collection){
+        if (o instanceof Collection) {
             return ((Collection) o).size() > 0;
         }
-        if(o instanceof Map){
+        if (o instanceof Map) {
             return ((Map) o).size() > 0;
         }
         return true;
@@ -33,38 +33,42 @@ public class CommonUtils {
 
     /**
      * 判断对象是否为空
+     *
      * @param o
      * @return
      */
-    public static boolean notExist(Object o){
+    public static boolean notExist(Object o) {
         return !exist(o);
     }
 
     /**
      * 判断字符串是否是纯数字
+     *
      * @param str
      * @return
      */
-    public static boolean isStringNumber(String str){
-        if(notExist(str)) return false;
+    public static boolean isStringNumber(String str) {
+        if (notExist(str)) return false;
         return str.matches("^[0-9]*$");
     }
 
     /**
      * 判断一个集合是否不存在非空子集
+     *
      * @param collection
      * @return
      */
-    public static boolean isSetEmpty(Collection collection){
+    public static boolean isSetEmpty(Collection collection) {
         return collection == null || collection.size() == 0;
     }
 
     /**
      * 判断一个集合是否存在非空子集
+     *
      * @param collection
      * @return
      */
-    public static boolean isSetNotEmpty(Collection collection){
+    public static boolean isSetNotEmpty(Collection collection) {
         return !isSetEmpty(collection);
     }
 
@@ -81,5 +85,22 @@ public class CommonUtils {
         if (t instanceof Date) return new SimpleDateFormat(DateFormatEnum.DEFAULT.getValue()).format(t);
         if (t instanceof Number || t instanceof Character || t instanceof Boolean) return String.valueOf(t);
         return JSON.toJSONString(t);
+    }
+
+    /**
+     * 得到一个Bean类型的所有字段名
+     *
+     * @param clazz
+     * @return
+     */
+    public static List<String> extractFieldNames(Class<?> clazz) {
+        Field[] fields = clazz.getDeclaredFields();
+        List<String> fieldNames = new ArrayList<String>();
+        if (fields.length > 0){
+            for (Field field : fields) {
+                fieldNames.add(field.getName());
+            }
+        }
+        return fieldNames;
     }
 }
